b5cc8be813a386876a47f170ed87185ebca804c4
manos <manosliverpool13@gmail.com>
2018-11-01 18:25:20 +0200

    second_change

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication2;
 
-/**
- *
- * @author ed-dev
- */
+
 public class JavaApplication2 {
 
     /**
@@ -16,6 +9,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("the name changed");
+        
     }
     
 }
