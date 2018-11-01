77422d5a001eb6f530341f35c9b875cd60d6ae2f
manos <manosliverpool13@gmail.com>
2018-11-01 18:17:18 +0200

    first_change

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -15,7 +15,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HJHKJHG);
+        System.out.println("the name changed");
     }
     
 }
