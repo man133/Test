8c5e9296e2f049d5c7b11aedc533673c9dd9e71d
manos <manosliverpool13@gmail.com>
2018-11-02 11:38:59 +0200

    name changed

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -1,6 +1,8 @@
-
 package javaapplication2;
 
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JavaApplication2 {
 
@@ -8,8 +10,19 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println ("ghgfdkjhkfdjh");
-        
+        System.out.println("Reading file");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/home/ed-dev/Documents/myJson.txt"));
+            String thisLine;
+            while ( (thisLine = br.readLine()) != null) {
+                System.out.println(thisLine);
+             }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+
     }
-    
+
 }
