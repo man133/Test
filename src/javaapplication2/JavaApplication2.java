311f1b0f7820d0cdc7db0f6687b8ef210c01932f
manos <manosliverpool13@gmail.com>
2018-11-02 17:58:46 +0200

    new commit

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/* new commit */
+/* this is new commit */
 public class JavaApplication2 {
 
     /**
@@ -36,22 +36,5 @@ public class JavaApplication2 {
     }
 
 }
-/**
- * BufferedReader Example
- */
-/*
-        System.out.println("Reading file");
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("/home/ed-dev/Documents/myJson.txt"));
-            String thisLine;
-            while ( (thisLine = br.readLine()) != null) {
-                System.out.println(thisLine);
-             }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(JavaApplication2.class.getName()).log(Level.SEVERE, null, ex);
-        } 
 
-    }
- */
+
