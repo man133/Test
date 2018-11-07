2dbc211247f1b19db64ac8a646fc2f88353cfa41
manos <manosliverpool13@gmail.com>
2018-11-07 11:02:03 +0200

    iss3 commit !!

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -7,6 +7,7 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
         System.out.println ("ghgfdkjhkfdjh");
         
